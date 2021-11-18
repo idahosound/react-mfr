b746069a67b01293ca374f8ba8f2444c7f45a33e
John McMahon <idahosound@gmail.com>
2021-11-18 09:54:53 -0700

    practice map, filter, reduce, find, findIndex

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map(function(x){
+//     return x * 2; //doubles the number
+// });
 
 //Filter - Create a new array by keeping the items that return true.
 
+// const newNumbers = numbers.filter(function(x){
+//     return x > 10; //returns numbers greater than 10
+// });
+
 //Reduce - Accumulate a value by doing something to each item in an array.
 
-//Find - find the first item that matches from an array.
+// const newNumbers = numbers.reduce(function(accumulator, x){
+//     console.log(accumulator);  // logs starting value
+//     return accumulator + x;   //  adds next number
+// });
 
-//FindIndex - find the index of the first item that matches.
+// //Find - find the first item that matches from an array.
+// const newNumbers = numbers.find(function(x){
+//     return x > 10;  // returns the first number greater than 10
+// });
+
+// //FindIndex - find the index of the first item that matches.
+// const newNumbers = numbers.findIndex(function(x){
+//     return x > 10;  // returns the array index value of the first number greater than 10
+// })
+
+// console.log(newNumbers);
+
